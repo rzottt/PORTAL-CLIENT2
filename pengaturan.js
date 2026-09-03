f8463b59198484279a8f421b3300ac83c687b979
rzottt <rezhaezot7@gmail.com>
2026-09-03 22:28:31 +0700

    Add files via upload

--- pengaturan.js
+++ pengaturan.js
@@ -5,9 +5,9 @@
 
 const CONFIG_APP = {
     // 1. Nama Studio Foto Anda (Akan tampil di header dan portal klien)
-    namaVendor: "LINIMASA VISUAL",
+    namaVendor: "Portal Client",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAVVZ24kiTeKOyrY_Zlg2boEovGb573Okg",
+    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
     whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
